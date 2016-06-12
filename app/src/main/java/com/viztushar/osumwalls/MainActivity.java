54fd25a9a6e5e37f1ecf5c1436466eddee70f049
viztushar <viztushar@gmail.com>
2016-06-12 15:50:03 +0530

    OsumWalls

--- app/src/main/java/com/viztushar/osumwalls/MainActivity.java
+++ app/src/main/java/com/viztushar/osumwalls/MainActivity.java
@@ -1,16 +1,28 @@
 package com.viztushar.osumwalls;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.viztushar.osumwalls.activities.FavWallaper;
 import com.viztushar.osumwalls.adapter.WallAdapter;
+import com.viztushar.osumwalls.fragments.HomeFragment;
 import com.viztushar.osumwalls.items.WallpaperItem;
 import com.viztushar.osumwalls.tasks.GetWallpapers;
 
@@ -21,55 +33,138 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GetWallpapers.Callbacks {
+public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    private RecyclerView recyclerView;
-    private ArrayList<WallpaperItem> items;
-    boolean scroll;
+    private NavigationView navigationView;
+    private DrawerLayout mDrawerLayout;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        items = new ArrayList<>();
-        new GetWallpapers(this,this).execute();
-
+        context = this;
+        switchFragment(new HomeFragment("walls"),false);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawerlayout);
+        navigationView = (NavigationView) findViewById(R.id.main_navigationview);
     }
 
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public void onListLoaded(String jsonResult) {
-        try {
-            if (jsonResult != null) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(jsonResult);
-                    JSONArray jsonMainNode = jsonResponse.optJSONArray("walls");
-                    for (int i = 0; i < jsonMainNode.length(); i++) {
-                        JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
-                        items.add(new WallpaperItem(jsonChildNode.optString("name"),
-                                jsonChildNode.optString("author"),
-                                jsonChildNode.optString("url"),
-                                jsonChildNode.optString("thumb")));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+    public void updateToggleButton(Toolbar toolbar) {
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+                syncState();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+                syncState();
+            }
+        };
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    public void setNav(Toolbar toolbar) {
+        updateToggleButton(toolbar);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_all:
+                        item.setChecked(true);
+                        HomeFragment fragment = new HomeFragment("walls");
+                        switchFragment(fragment, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.navigation_new:
+                        item.setChecked(true);
+                        HomeFragment fragment1 = new HomeFragment("walls2");
+                        switchFragment(fragment1, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_favorites:
+                        item.setChecked(true);
+                        Intent intent=new Intent(getApplicationContext(), FavWallaper.class);
+                        startActivity(intent);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_material:
+                        item.setChecked(true);
+                        HomeFragment fragment3 = new HomeFragment("walls2");
+                        switchFragment(fragment3, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_landscape:
+                        item.setChecked(true);
+                        HomeFragment fragment4 = new HomeFragment("walls");
+                        switchFragment(fragment4, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_nature:
+                        item.setChecked(true);
+                        HomeFragment fragment5 = new HomeFragment("walls");
+                        switchFragment(fragment5, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_sea:
+                        item.setChecked(true);
+                        HomeFragment fragment6 = new HomeFragment("walls2");
+                        switchFragment(fragment6, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_city:
+                        item.setChecked(true);
+                        HomeFragment fragment7 = new HomeFragment("walls");
+                        switchFragment(fragment7, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_vintage:
+                        item.setChecked(true);
+                        HomeFragment fragment8 = new HomeFragment("walls2");
+                        switchFragment(fragment8, true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_about:
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                }
+                return true;
+            }
+        });
         }
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-
-        recyclerView.setAdapter(new WallAdapter(this,items));
+    public void switchFragment(Fragment fragment, boolean b) {
+        if (!b)
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                    .replace(R.id.fragment_holder, fragment).commit();
+        else
+            getSupportFragmentManager().beginTransaction().addToBackStack(null)
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                    .replace(R.id.fragment_holder, fragment).commit();
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/viztushar/osumwalls/activities/FavWallaper.java
+++ app/src/main/java/com/viztushar/osumwalls/activities/FavWallaper.java
@@ -1,6 +1,5 @@
 package com.viztushar.osumwalls.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,13 +14,17 @@ import com.viztushar.osumwalls.items.WallpaperItem;
 import com.viztushar.osumwalls.others.Preferences;
 import com.viztushar.osumwalls.tasks.GetWallpapers;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
  * Created by Tushar on 06-06-2016.
  */
 
-public class FavWallaper extends AppCompatActivity  {
+public class FavWallaper extends AppCompatActivity implements GetWallpapers.Callbacks {
 
     private Toolbar toolbar;
     private RecyclerView recyclerView;
@@ -33,21 +36,45 @@ public class FavWallaper extends AppCompatActivity  {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fav_wall);
-        mPre=new Preferences(this);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mPre = new Preferences(this);
+        toolbar = (Toolbar) findViewById(R.id.fav_toolbar);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        items = new ArrayList<>();
+        new GetWallpapers(this, this).execute();
 
-        if(items != null){
 
-            mAdapter = new WallAdapter(this,items);
-
-            recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-            RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
-            recyclerView.setLayoutManager(mLayoutManager);
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-            recyclerView.setAdapter(new WallAdapter(this,items));
-
-        }
     }
 
+    @Override
+    public void onListLoaded(String jsonResult) {
+        try {
+            if (jsonResult != null) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(jsonResult);
+                    JSONArray jsonMainNode = jsonResponse.optJSONArray("walls");
+                    for (int i = 0; i < jsonMainNode.length(); i++) {
+                        JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                        items.add(new WallpaperItem(jsonChildNode.optString("name"),
+                                jsonChildNode.optString("author"),
+                                jsonChildNode.optString("url"),
+                                jsonChildNode.optString("thumb")));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_fav);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        recyclerView.setAdapter(new WallAdapter(this, items));
+    }
 }
